54f28ea2bbfe08ac028c4d77da7bf0e090bf64fc
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2023-10-05 23:06:29 -0300

    codigo comentado por completo

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, send_from_directory, redirect, make_response, render_template
 import json
 
+# Retorna uma mensagem de erro em forma de HTML
 def erro_html( mensagem ):
     return make_response( f"<p style=\"color:red;\">{mensagem}</p>\<a href=\"/login/index.html\">Voltar</a>" )
 
+# Reescreve um arquivo em dicionario/lista python para JSON
 def escrever_arquivo( caminho, data ):
     data_arquivo = open( caminho, "w" )
     texto = json.dumps( data )
     data_arquivo.write( texto )
     data_arquivo.close()
 
+# Le um arquivo como JSON para dicionario/lista python
 def abrir_arquivo( caminho ):
     data_arquivo = open( caminho )
     texto = data_arquivo.read()
@@ -90,7 +93,17 @@ def login():
 
 #! Realiza cadastro de novos usuarios
 '''
-	
+	Pega o conteudo do arquivo de usuarios
+
+    cria um dicionario com as informaçoes do novo usuario
+		- usuario : puxado da pagina html
+        - senha : puxado da pagina html
+        - admin : puxado da pagina html
+        - agendamento : por padrao uma lista vazia
+
+    adiciona o novo usuario a lista de usuarios
+    reescreve o arquivo de usuarios
+    redireciona para a pagina de login
 '''
 @app.route( "/cadastre", methods=["POST"] )
 def cadastro():
@@ -98,11 +111,12 @@ def cadastro():
     novo_usuario = {
 		"usuario": request.form.get("usuario"),
 		"senha": request.form.get("senha"),
-		"admin": request.form.get("admin") == "on"
+		"admin": request.form.get("admin") == "on",
+        "agendamento": []
 	}
     usuarios.append( novo_usuario )
     escrever_arquivo( "data/usuario.json", usuarios )
-    return redirect( "/index.html", code=302 )
+    return redirect( "/", code=302 )
 
 #! Sai do site e apaga os cookies
 '''
@@ -115,6 +129,31 @@ def sair():
     resposta.delete_cookie( "nome_usuario" )
     return resposta
 
+#! Retorna todos os livros
+'''
+	pega os dados dos cookies;
+    le o arquivo de usuarios
+
+    realiza um looping para verificar se os
+    dados dos cookies estao na lista de usuarios
+
+    #! Caso nao esteja retorna o erro "Usuario nao encontrado"
+
+    pega as informaçoes de procura do html
+    verifica se proc existe e nao e um texto vazio
+
+		#! Caso contrario retorna os dados de todos os livros
+
+		transforma os dados de procura em um texto minusculo
+        cria uma lista vazia
+
+        cria um loop para verificar aqueles dados dos livros
+		com titulo semelhantes aos dados de procura
+        e adiciona esses dados a lista vazia
+        
+        renderiza uma pagina html com os dados dos livro encontrados
+
+'''
 @app.route( "/livros", methods=["GET","POST"] )
 def get_livros():
     cookie_usuario = request.cookies.get("nome_usuario")
@@ -138,6 +177,26 @@ def get_livros():
 
     return erro_html( "Usuario não encontrado" )
 
+#! Sistema de agendamento
+'''
+	pega o parametro chamado ID
+
+	pega os dados dos cookies
+    pega os dados do arquivo de usuarios
+
+    faz um loop para verificar se o usuario existe
+		#! Caso contrario retorna o erro "Usuario nao encontrado
+        
+        pega o arquivo de livros
+        faz um loop para encontrar aquele livro com o ID igual	
+        ao parametro ID
+        
+        Quando encontrar inverte o valor de "disponivel"
+		
+        adiciona esse ID a lista de agendamentos do usuario
+        atualiza o arquivo de usuarios
+        atualiza a pagina
+'''
 @app.route( "/agendar/<int:id>", methods=["GET"] )
 def agendar( id ):
     cookie_usuario = request.cookies.get("nome_usuario")
@@ -145,9 +204,6 @@ def agendar( id ):
 
     for usu in usuarios:
         if usu.get("usuario") == cookie_usuario:
-
-            if len( usu.get("agendamento") ) >= 2:
-                return erro_html( "Você já agendou o máximo de livros (2)" )
 
             livros = abrir_arquivo( "data/livros.json" )
             for liv in livros:
@@ -163,6 +219,23 @@ def agendar( id ):
 
     return erro_html( "Usuario não encontrado" )
 
+#! Le cada livro
+'''
+	pega o parametro ID do livro
+    pega os dados dos cookies
+    pega o arquivo dos usuario
+
+    faz um loop para verificar se o usuario
+    dos cookies existe
+		#! Caso contrario retorna o erro "Usuario nao encontrado"
+
+        le o arquivo de livros
+        Encontra entre os livros aquele com o ID igual
+        ao parametro
+
+        encerra o loop
+        retorna uma pagina html com os dados do livro
+'''
 @app.route( "/livros/id/<int:id>", methods=["GET"] )
 def get_livros_por_id( id ):
     cookie_usuario = request.cookies.get("nome_usuario")
@@ -182,6 +255,32 @@ def get_livros_por_id( id ):
 
     return redirect( "/login/index.html", code=301 )
 
+#! Adicionar um unico livro
+'''
+	pega os dados dos cookies
+    pega o arquivo dos usuarios
+
+    faz um loop para verificar se o usuario
+    dos cookies existe
+		#! Caso contrario retorna o erro "Usuario nao encontrado"
+        
+        verifica se o usuario encontrado e Administrador
+        #! Caso contrario retorna o erro "Você não tem permissão para cadastrar livros"
+        
+        encontra o maior ID entre os livros
+        
+        cria um novo livro em forma de dicionario
+			- id : maior ID
+            - disponivel : True por padrao
+            - titulo : dados do forms
+            - autor : dados do forms
+        
+		abre o arquivo de livros
+        adiciona o novo livro a lista de livros
+        reescreve arquivo de livros
+        
+    redireciona para a pagina da lista de livros
+'''
 @app.route( "/add-livro", methods=["POST"] )
 def add_livro():
     cookie_usuario = request.cookies.get("nome_usuario")
@@ -193,6 +292,7 @@ def add_livro():
             if usu.get("admin") == False:
                 return erro_html( "Você não tem permissão para cadastrar livros" )
 
+			# encontrar o maior numero
             maior = -1
             for d in abrir_arquivo( "data/livros.json" ):
                 if d.get("id") > maior:
@@ -211,5 +311,6 @@ def add_livro():
 
     return redirect( "/livros", code=301 )
 
+# Roda toda a aplicaçao
 if __name__ == "__main__":
     app.run( host="0.0.0.0", port=3000, debug=True )
